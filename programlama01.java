276501733c59c34af905ec381aa270c5f49a972c
nehir <nehirrass@gmail.com>
2024-10-10 16:10:53 +0300

    Create programlama01.java

---
+++ programlama01.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class programlama01 {
+    public static void main(String[] args) {
+        
+        Scanner inp = new Scanner(System.in);
+
+        // 1 Program to validate a Date
+        
+        System.out.println("Enter day:  ");
+        int a = inp.nextInt();
+        System.out.println("Enter month:  ");
+        int a = inp.nextInt();
+        System.out.println("Enter year:  ");
+        int a = inp.nextInt();
+
+        
+    }
+}

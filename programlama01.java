6f82170dae7c553ee10f27df47033b3046ef5de4
nehir <nehirrass@gmail.com>
2024-10-10 17:16:38 +0300

    Update programlama01.java
    problem 1

--- programlama01.java
+++ programlama01.java
@@ -2,18 +2,31 @@ import java.util.Scanner;
 
 public class programlama01 {
     public static void main(String[] args) {
-        
+
         Scanner inp = new Scanner(System.in);
 
         // 1 Program to validate a Date
         
         System.out.println("Enter day:  ");
-        int a = inp.nextInt();
+        int day = inp.nextInt();
         System.out.println("Enter month:  ");
-        int a = inp.nextInt();
+        int month = inp.nextInt();
         System.out.println("Enter year:  ");
-        int a = inp.nextInt();
-
+        int year = inp.nextInt(); 
         
+        int x = 28;
+        if((year % 4 == 0)&&(year % 100 != 0)||(year % 400 == 0)){
+            x=29;
+        }
+        int[] aylar = {31,x,31,30,31,30,31,31,30,31,30,31};
+
+        if (aylar[month-1]>= day) {
+            System.out.println("This is a valid date.");
+        } else {
+            System.out.println("Not a valid date.");
+        }
+
+        //
+
     }
 }
